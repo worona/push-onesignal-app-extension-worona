7eb2b74bc95e4ac3795df3b0f33d8ad839e12d52
Luis Herranz <luisherranz@gmail.com>
2017-03-29 12:57:16 +0200

    Create first saga with alert and console.log

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,18 +1,17 @@
-import { fork } from 'redux-saga/effects';
+/* eslint-disable no-undef */
+import { fork, select } from 'redux-saga/effects';
 
 function* initOnesignal() {
   const appId = yield select(deps.selectorCreators.getSetting('pushOnesignal', 'appId'));
   window.plugins.OneSignal
-  .startInit(appId)
-  .handleNotificationOpened(function(jsonData) {
-    alert("Notification opened:\n" + JSON.stringify(jsonData));
-    console.log('didOpenRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-  })
-  .endInit();
+    .startInit(appId)
+    .handleNotificationOpened(jsonData => {
+      alert(`Notification opened:\n${JSON.stringify(jsonData)}`);
+      console.log(`didOpenRemoteNotificationCallBack: ${JSON.stringify(jsonData)}`);
+    })
+    .endInit();
 }
 
 export default function* pushOnesignalSagas() {
-  yield [
-    fork(initOnesignal),
-  ];
+  yield [fork(initOnesignal)];
 }
